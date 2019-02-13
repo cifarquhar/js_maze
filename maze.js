dbd2f7105901d2b890261c374e165ff97b74ba55
cifarquhar <colin.farquhar@codeclan.com>
2019-02-13 12:02:59 +0000

    add wall rendering

--- maze.js
+++ maze.js
@@ -7,6 +7,20 @@ const ctx = canvas.getContext("2d");
 let playerX = 20;
 let playerY = 20;
 
+// Walls
+const wallPoints = [
+  {
+    xCoord: 50,
+    yCoord: 50,
+    direction: "H"
+  },
+  {
+    xCoord: 100,
+    yCoord: 100,
+    direction: "V"
+  }
+]
+
 
 // INPUT HANDLING
 
@@ -43,10 +57,27 @@ function drawPlayer(){
   ctx.closePath();
 }
 
+// Walls
+function drawWalls(){
+  wallPoints.forEach((wall) => {
+    ctx.beginPath();
+    ctx.moveTo(wall.xCoord, wall.yCoord);
+    if (wall.direction === "H"){
+      ctx.lineTo(wall.xCoord + 20, wall.yCoord);
+    }
+    else if (wall.direction === "V"){
+      ctx.lineTo(wall.xCoord, wall.yCoord + 20);
+    }
+    ctx.strokeStyle = "#2D0E00";
+    ctx.stroke();
+  });
+}
+
 // Rendering turn
 function draw(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawPlayer();
+  drawWalls();
   requestAnimationFrame(draw);
 }
 
